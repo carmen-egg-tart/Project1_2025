5966f6335988dcf22d3c699352fb0592bb40ab52
Yuchen Zhang <33946014522qq.com>
2025-04-10 14:49:23 +0100

    Add an element of surprise; resolves zyc #1

--- reaction.py
+++ reaction.py
@@ -1,8 +1,11 @@
-from gpiozero import LED,Button
+from gpiozero import LED, Button
 from time import sleep
+from random import uniform
 
 led = LED(4)
 
 led.on()
-sleep(5)
+
+sleep(uniform(5, 10))
+
 led.off()
